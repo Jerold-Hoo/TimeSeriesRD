f4094c3cdf6af26284187f09e20c054dc56c1657
ZhouXinQin <1570149578@qq.com>
2019-10-28 18:01:02 +0800

    add plot arima at linepay 10.28

--- ARIMA.py
+++ ARIMA.py
@@ -6,6 +6,9 @@
 # @Description: use Arima
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
 data = pd.read_csv(r'KWH_Data\train.csv')
 data = data[['time','KWH']]
@@ -15,7 +18,15 @@ data = data['KWH']
 def draw_SMAtrend(timeseries, size):
     rol_mean = timeseries.rolling(window=size).mean()
     rol_std = timeseries.rolling(window=size).std()
-    print(rol_mean.head(50))
-    print(rol_std.head(50))
 
-draw_SMAtrend(data, 10)
+    #print(rol_mean.head())
+    #plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y %H'))
+    #plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+
+    plt.plot(timeseries.index,timeseries.values)
+    plt.plot(rol_mean.index, rol_mean.values)
+
+    #plt.gcf().autofmt_xdate()
+    plt.show()
+
+draw_SMAtrend(data, 24)
